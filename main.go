a805f78bbc9af4cdc79a73f88cb2ee390d92c90c
Shreyash Desai <shreyashdesai31796@gmail.com>
2024-08-04 23:20:43 +0530

    4 Aug 2024

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var ans int
+	var key int
+	nums := [4] int {2 , -1 , 12 , 23}
+
+
+	for i := 0 ; i<len(nums) ; i++ {
+		if nums[i]  {
+
+		} 
+	}
+
+	fmt.Println("ARRAY : " , nums)
+	fmt.Println("ANSWER : ",ans)
+
+}
